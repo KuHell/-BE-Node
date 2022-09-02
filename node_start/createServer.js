58d758c8619160c66eeb5b760e4793af3a0e64bc
KuHell <itkdgus489@gmail.com>
2022-09-02 14:22:47 +0900

    first commit

---
+++ node_start/createServer.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  res.write("<h1>Hello node</h1>");
+  res.end("<p>hello</p>");
+});
+
+server.listen(3000);
+
+server.on("listening", () => {
+  console.log("3000 port is running");
+});
+
+server.on("error", (error) => {
+  console.log("error:", error);
+});
