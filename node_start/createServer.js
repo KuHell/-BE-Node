fb3475878d8bdc003e1e3914452ec773a6d8a5f6
KuHell <itkdgus489@gmail.com>
2022-09-04 04:20:49 +0900

    0904

--- node_start/createServer.js
+++ node_start/createServer.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
   res.write("<h1>Hello node</h1>");
-  res.end("<p>go Server</p>");
+  res.end("<p>go Server 09 04</p>");
 });
 
 server.listen(3000);
