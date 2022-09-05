bc356233b83873532179662e4446858f789a89db
KuHell <itkdgus489@gmail.com>
2022-09-06 00:35:16 +0900

    2개의 포트

--- node_start/createServer.js
+++ node_start/createServer.js
@@ -1,17 +1,23 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-  res.write("<h1>Hello node</h1>");
-  res.end("<p>go Server 09 04</p>");
-});
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.write("<h1>Hello node</h1>");
+    res.end("<p>go Server 09 04</p>");
+  })
 
-server.listen(3000);
+  .listen(3000, () => {
+    console.log("3000 port is running");
+  });
 
-server.on("listening", () => {
-  console.log("3000 port is running");
-});
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+    res.write("<h1>Hello node</h1>");
+    res.end("<p>go Server 09 04</p>");
+  })
 
-server.on("error", (error) => {
-  console.log("error:", error);
-});
+  .listen(3001, () => {
+    console.log("3001 port is running");
+  });
