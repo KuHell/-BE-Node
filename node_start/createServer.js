e7ce065d0a47dfd3c1836a8ffacb9126019731fd
KuHell <itkdgus489@gmail.com>
2022-09-06 00:52:28 +0900

    add: http, createServer

--- node_start/createServer.js
+++ node_start/createServer.js
@@ -1,23 +1,18 @@
 const http = require("http");
+const fs = require("fs").promises;
 
 http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.write("<h1>Hello node</h1>");
-    res.end("<p>go Server 09 04</p>");
+  .createServer(async (req, res) => {
+    try {
+      const data = await fs.readFile("./index.html");
+      res.writeHead(200, { "Content-Type": "text/html; charset-utf-8" });
+      res.end(data);
+    } catch (err) {
+      console.error(err);
+      res.writeHead(500, { "Content-Type": "text/html; charset=utf-8" });
+      res.end(err.message);
+    }
   })
-
   .listen(3000, () => {
     console.log("3000 port is running");
-  });
-
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.write("<h1>Hello node</h1>");
-    res.end("<p>go Server 09 04</p>");
-  })
-
-  .listen(3001, () => {
-    console.log("3001 port is running");
   });
